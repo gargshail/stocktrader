9274e1b53c65a01b5041495cf497fafaaf7bf604
Shailesh Garg <garg.shailesh@gmail.com>
2021-10-06 22:47:00 -0700

    Add charts, up down count

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,6 +8,7 @@ import pandas as pd
 import random
 import datetime
 import numpy
+import matplotlib.pyplot as plt
 
 
 # all_stocks = api.list_assets(status='active')
@@ -16,6 +17,17 @@ import numpy
 #                                                        or ticker.name.lower().find('fund') != -1 
 #                                                       ) ]
 
+def bar_to_df(bars) :
+    df = pd.DataFrame({
+        'open':[bar.o for bar in bars],
+        'high':[bar.h for bar in bars],
+        'low':[bar.l for bar in bars],
+        'close':[bar.c for bar in bars],
+        'volume':[bar.v for bar in bars]
+    }, index=[bar.t for bar in bars])
+    return df
+
+api = tradeapi.REST()
 st.write("Test 1 ")
 url = 'https://raw.githubusercontent.com/datasets/s-and-p-500-companies/master/data/constituents.csv'
 sp500 = pd.read_csv(url, index_col=0)
@@ -26,4 +38,28 @@ for ticker in tickers_for_chart:
     st.image(f"https://finviz.com/chart.ashx?t={ticker}&ta=1&p=d&s=m&rev={random.random()*1000}") 
     
 
+tickers = sp500.index
+batch_size = 100
+chunked = [tickers[i * batch_size:(i + 1) * batch_size] for i in range((len(tickers) + batch_size - 1) // batch_size )]
+data = {}
+for chunk in chunked:
+    bars = api.get_barset(chunk, "day", limit=2)
+    for ticker in chunk:
+        data[ticker] = bar_to_df(bars[ticker])
+ticker_summary = pd.DataFrame(index=tickers)
+ticker_summary['todays_change'] = pd.Series({ticker: data[ticker].tail(2).close.pct_change().iloc[1]*100 for ticker in tickers})
+
+fig = plt.figure()
+ax = fig.add_subplot(1,1,1)
+sorted_data = ticker_summary.todays_change[tickers].sort_values(ascending=False)
+ax.bar(sorted_data.index, sorted_data)
+st.write(fig)
+
+st.dataframe(ticker_summary)
+
+up_count = ticker_summary.query('todays_change > 0').count()[0]
+down_count = ticker_summary.query('todays_change < 0').count()[0]
+
+st.markdown(f"Up:{up_count}")
+st.markdown(f"Down:{down_count}")
 
